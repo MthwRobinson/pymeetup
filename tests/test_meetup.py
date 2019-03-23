0e7459e23865fcbdff8921b1bdca560b57a5c7a7
matt <mthw.wm.robinson@gmail.com>
2019-03-23 14:41:46 -0400

    added tests for events and rsvps

--- tests/test_meetup.py
+++ tests/test_meetup.py
@@ -10,6 +10,23 @@ class FakeResponse:
         self.text = '{"parrot": "big_beak"}'
         self.status_code = status_code
 
+def test_get_events(monkeypatch):
+    fake_response = FakeResponse(200)
+    monkeypatch.setattr('requests.get', lambda x: fake_response)
+    meetup = Meetup(key='fake_key')
+    events = meetup.get_events('fake_group', page=1000, 
+                               scroll='future_or_past',
+                               no_earlier_than='2010-05-02',
+                               no_later_than='2019-04-01')
+    assert isinstance(events, dict)
+
+def test_get_event_rsvps(monkeypatch):
+    fake_response = FakeResponse(200)
+    monkeypatch.setattr('requests.get', lambda x: fake_response)
+    meetup = Meetup(key='fake_key')
+    events = meetup.get_event_rsvps('fake_group', 8675309, response='yes')
+    assert isinstance(events, dict)
+
 def test_meepup_does_not_warn_if_good_status(monkeypatch):
     fake_response = FakeResponse(200)
     monkeypatch.setattr('requests.get', lambda x: fake_response)
